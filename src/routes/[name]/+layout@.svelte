8ed9e8466f1ef14df9b2c73a716b348a4c516f3e
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2022-12-28 06:37:08 -0300

    Improved navbar

--- src/routes/[name]/+layout@.svelte
+++ src/routes/[name]/+layout@.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
-	export let data: LayoutData;
+	import { page } from '$app/stores';
 
-	import { signOut } from '@auth/sveltekit/client';
+	export let data: LayoutData;
 </script>
 
 <header class="fixed top-0 left-0 w-full h-14 flex items-center justify-between py-2 bg-white z-50">
-	<a href="/" class="h-full">
+	<a href="/{$page.url.pathname.split('/').slice(1, 3).join('/')}{$page.url.search}" class="h-full">
 		<img src="/favicon.png" alt="Logo" class="aspect-square h-full" />
 	</a>
 	<div class="flex gap-2 items-center justify-end mr-2">
@@ -14,10 +14,12 @@
 			<p>
 				<strong>@{data.session?.user?.name}</strong>
 			</p>
-			<button on:click={() => signOut()} class="button">Sign out</button>
+			<a href="/{$page.url.pathname.split('/').at(1)}">Change repo</a>
 		</div>
 		{#if data.session?.user?.image}
-			<img src={data.session.user.image} alt="Avatar" class="aspect-square w-10 rounded-full" />
+			<a href="/user" class="w-fit h-fit">
+				<img src={data.session.user.image} alt="Avatar" class="aspect-square w-10 rounded-full" />
+			</a>
 		{/if}
 	</div>
 </header>
