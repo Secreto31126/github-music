74a2a110ee57616e9bf1ee00b509278277aed7d1
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2022-12-29 12:07:17 -0300

    Bug fixes

--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import type { Song } from './types';
+
 	import { browser } from '$app/environment';
 	import { getParentPath } from '$lib/paths';
+	import { loop_type } from '$lib/stores';
+	import { onDestroy, onMount } from 'svelte';
 
 	export let index = 0;
 	export let songs: Array<Song> | null;
@@ -10,8 +13,6 @@
 	let retries = 0;
 	let last_requested: string;
 	let url = null as string | null;
-
-	setupAudio();
 
 	let name_strcpy: string;
 	$: if (songs) {
@@ -51,6 +52,8 @@
 	}
 
 	let loop_list = false;
+	let loop_song = false;
+
 	function previous() {
 		let temp = index - 1;
 		if (temp < 0) {
@@ -68,7 +71,7 @@
 		let temp = index + 1;
 		if (loop_list) {
 			temp %= songs?.length || 1;
-		} else if ((songs?.length || 0) < temp) {
+		} else if ((songs?.length || 0) <= temp) {
 			return;
 		}
 
@@ -77,20 +80,22 @@
 
 	function loop() {
 		// 00 default -> 01 loop song
-		if (!player.loop && !loop_list) {
+		if (!loop_song && !loop_list) {
 			loop_list = true;
+			$loop_type = 1;
 		}
 
 		// 01 loop list -> 10 loop song
-		else if (!player.loop && loop_list) {
-			player.loop = true;
+		else if (!loop_song && loop_list) {
+			loop_song = true;
 			loop_list = false;
+			$loop_type = 2;
 		}
 
 		// 10 loop song -> 00 default
 		else {
-			loop_list = false;
-			player.loop = false;
+			loop_song = false;
+			$loop_type = 0;
 		}
 	}
 
@@ -150,92 +155,116 @@
 		}
 	}
 
+	onMount(() => {
+		if ($loop_type == 1) {
+			loop_list = true;
+		}
+
+		if ($loop_type == 2) {
+			loop_song = true;
+		}
+
+		setupAudio();
+	});
+
+	// Copilot insisted on this
+	onDestroy(() => {
+		if (player) {
+			player.pause();
+			player.src = '';
+		}
+	});
+
 	$: index, setupAudio();
+
+	$: if (browser && player) {
+		player.loop = loop_song;
+	}
 </script>
 
-{#if songs}
-	<div class="flex justify-center items-center bg-orange-100 w-full h-full gap-2">
-		<img
-			src={songs[index].cover || '/favicon.png'}
-			alt="Cover"
-			on:error={missingImg}
-			class="aspect-square h-2/3"
-		/>
-		<div class="flex flex-col w-1/4">
-			<a href="{getParentPath(origin.pathname, 1)}{origin.search}">{name_strcpy}</a>
-			{#key url}
-				{#if url}
-					<audio
-						bind:this={player}
-						on:play={() => (playing = true)}
-						on:pause={() => (playing = false)}
-						on:ended={next}
-						autoplay
-						preload="auto"
-					>
-						<source src={url} type="audio/mpeg" on:error={setupAudio} />
-						Your browser does not support the audio element.
-					</audio>
+<div class="flex justify-center items-center bg-orange-100 w-full h-full gap-2">
+	<img
+		src={songs?.[index].cover || '/favicon.png'}
+		alt="Cover"
+		on:error={missingImg}
+		class="aspect-square h-2/3"
+	/>
+	<div class="flex flex-col w-1/4">
+		<a href="{getParentPath(origin.pathname, 1)}{origin.search}">{name_strcpy}</a>
+		{#key url}
+			{#if url}
+				<audio
+					bind:this={player}
+					on:play={() => (playing = true)}
+					on:pause={() => (playing = false)}
+					on:ended={next}
+					autoplay
+					preload="auto"
+				>
+					<source src={url} on:error={setupAudio} on:canplay={() => (retries = 0)} />
+					Your browser does not support the audio element.
+				</audio>
 
-					<div class="flex justify-evenly [&>*]:w-6">
-						<button on:click={previous}>
+				<div class="flex justify-evenly [&>*]:w-6">
+					<button on:click={previous}>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+							<path fill="#000" d="M9 18L21 7v9.166L31 7v22l-10-9.167V29zm0 11H5V7h4z" />
+						</svg>
+					</button>
+
+					<button on:click={stop} title="Stop">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+							<path fill="#000" d="M7 7h22v22H7z" />
+						</svg>
+					</button>
+
+					<button on:click={toggle} title={playing ? 'Pause' : 'Play'}>
+						{#if playing}
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-								<path fill="#000" d="M9 18L21 7v9.166L31 7v22l-10-9.167V29zm0 11H5V7h4z" />
+								<path fill="#000" d="M20 7h5v22h-5zm-9 0h5v22h-5z" />
 							</svg>
-						</button>
-
-						<button on:click={stop} title="Stop">
+						{:else}
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-								<path fill="#000" d="M7 7h22v22H7z" />
+								<path fill="#000" d="M8 7l22 11L8 29z" />
 							</svg>
-						</button>
+						{/if}
+					</button>
 
-						<button on:click={toggle} title={playing ? 'Pause' : 'Play'}>
-							{#if playing}
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-									<path fill="#000" d="M20 7h5v22h-5zm-9 0h5v22h-5z" />
-								</svg>
-							{:else}
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-									<path fill="#000" d="M8 7l22 11L8 29z" />
-								</svg>
-							{/if}
-						</button>
-
-						<button on:click={loop} title="Loop">
-							{#if !player?.loop}
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-									<!-- <path fill="#3B88C3" d="M36 32c0 2.209-1.791 4-4 4H4c-2.209 0-4-1.791-4-4V4c0-2.209 1.791-4 4-4h28c2.209 0 4 1.791 4 4v28z"/> -->
-									<path
-										fill="#{loop_list ? '0F0' : '000'}"
-										d="M30.2 10L23 4v4h-8C9.477 8 5 12.477 5 18c0 1.414.297 2.758.827 3.978l3.3-2.75C9.044 18.831 9 18.421 9 18c0-3.314 2.686-6 6-6h8v4l7.2-6zm-.026 4.023l-3.301 2.75c.083.396.127.806.127 1.227 0 3.313-2.687 6-6 6h-8v-4l-7.2 6 7.2 6v-4h8c5.522 0 10-4.478 10-10 0-1.414-.297-2.758-.826-3.977z"
-									/>
-								</svg>
-							{:else}
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-									<path
-										fill="#0F0"
-										d="M30.174 14.023l-3.301 2.75c.083.396.127.806.127 1.227 0 3.313-2.687 6-6 6h-1.059c.037.329.059.662.059 1 0 1.054-.19 2.06-.523 3H21c5.522 0 10-4.478 10-10 0-1.414-.297-2.758-.826-3.977zM30.2 10L23 4v4h-8C9.477 8 5 12.477 5 18c0 .099.012.194.015.292 1.186-1.059 2.649-1.812 4.273-2.125C10.062 13.75 12.326 12 15 12h8v4l7.2-6z"
-									/>
-									<circle fill="#0F0" cx="11" cy="25" r="6" />
-									<path
-										fill="#FFF"
-										d="M10.201 23.116h-.708c-.576 0-.815-.42-.815-.828 0-.419.3-.827.815-.827h1.703c.516 0 .804.371.804.852v5.752c0 .6-.384.935-.899.935s-.899-.336-.899-.935v-4.949z"
-									/>
-								</svg>
-							{/if}
-						</button>
-
-						<button on:click={next}>
+					<button on:click={loop} title="Loop">
+						{#if !player?.loop}
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-								<path fill="#000" d="M27 18L15 7v9.166L5 7v22l10-9.167V29zm0-11h4v22h-4z" />
+								<!-- <path fill="#3B88C3" d="M36 32c0 2.209-1.791 4-4 4H4c-2.209 0-4-1.791-4-4V4c0-2.209 1.791-4 4-4h28c2.209 0 4 1.791 4 4v28z"/> -->
+								<path
+									fill="#{loop_list ? '0F0' : '000'}"
+									d="M30.2 10L23 4v4h-8C9.477 8 5 12.477 5 18c0 1.414.297 2.758.827 3.978l3.3-2.75C9.044 18.831 9 18.421 9 18c0-3.314 2.686-6 6-6h8v4l7.2-6zm-.026 4.023l-3.301 2.75c.083.396.127.806.127 1.227 0 3.313-2.687 6-6 6h-8v-4l-7.2 6 7.2 6v-4h8c5.522 0 10-4.478 10-10 0-1.414-.297-2.758-.826-3.977z"
+								/>
 							</svg>
-						</button>
-					</div>
-				{:else}
-					<p>Failed to get URL</p>
-				{/if}
-			{/key}
-		</div>
+						{:else}
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+								<path
+									fill="#0F0"
+									d="M30.174 14.023l-3.301 2.75c.083.396.127.806.127 1.227 0 3.313-2.687 6-6 6h-1.059c.037.329.059.662.059 1 0 1.054-.19 2.06-.523 3H21c5.522 0 10-4.478 10-10 0-1.414-.297-2.758-.826-3.977zM30.2 10L23 4v4h-8C9.477 8 5 12.477 5 18c0 .099.012.194.015.292 1.186-1.059 2.649-1.812 4.273-2.125C10.062 13.75 12.326 12 15 12h8v4l7.2-6z"
+								/>
+								<circle fill="#0F0" cx="11" cy="25" r="6" />
+								<path
+									fill="#FFF"
+									d="M10.201 23.116h-.708c-.576 0-.815-.42-.815-.828 0-.419.3-.827.815-.827h1.703c.516 0 .804.371.804.852v5.752c0 .6-.384.935-.899.935s-.899-.336-.899-.935v-4.949z"
+								/>
+							</svg>
+						{/if}
+					</button>
+
+					<button on:click={next}>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+							<path fill="#000" d="M27 18L15 7v9.166L5 7v22l10-9.167V29zm0-11h4v22h-4z" />
+						</svg>
+					</button>
+				</div>
+			{:else if retries < 3}
+				<p>Loading...</p>
+			{:else}
+				<p>Failed to get URL</p>
+			{/if}
+		{/key}
 	</div>
-{/if}
+</div>
