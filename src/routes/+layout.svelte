8484afcbb6d4669aa2370f448642418faeaf2d66
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-02-19 02:52:12 -0300

    Update +layout.svelte
    Signed-off-by: Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,11 @@
 <div class="w-full">
 	<slot />
 </div>
+
+a {
+	-webkit-tap-highlight-color: rgba(0,0,0,0);
+	-webkit-user-select: none;
+	-khtml-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+}
